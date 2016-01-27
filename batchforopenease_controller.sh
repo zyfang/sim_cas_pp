3e4fffbfcb65dcd73d7c86eae9def31a95efa447
zyfang <yzfang1@gmail.com>
2016-01-27 17:21:52 +0100

    track batchforopenease for controller script

--- batchforopenease_controller.sh
+++ batchforopenease_controller.sh
@@ -1,5 +1,5 @@
-DATA_IN_DIR_FULLPATH="/media/yfang/hdd/HumanData/ExperimentSubjectsData/ferko_run_1/"
-DBNAME="ferko_run_1"
+DATA_IN_DIR_FULLPATH="/media/yfang/hdd/ControllerData/runrecord2_20150914_newprocessdelay_next150/"
+DBNAME="simtest200_b"
 counter=0
 
 echo $DBNAME
@@ -7,11 +7,12 @@ for D in `find $DATA_IN_DIR_FULLPATH -type d`
 do
 	filepath=$D"/state.log" #the loop goes through every subfolder so at some point it will be at the right level
 	#collectionname=`echo $filepath | cut -d '_' -f 4``echo $filepath | cut -d '_' -f 5`
-	re="\/.*\/.*\/.*_(mug.*_.*)\/gzserver\/state\.log"
+	re="\/.*\/.*\/(.*)\/gzserver\/state\.log"
 	if [[ $filepath =~ $re ]]; then 
 
 		collectionname=${BASH_REMATCH[1]};
-		#echo $filepath" to "$collectionname
+		collectionname=${collectionname//[^[:alnum:]]/}
+		echo $filepath" to "$collectionname
 		gzserver --verbose -s libPostProcess.so -p $filepath --db $DBNAME --collection $collectionname --suffix $counter -replaying
 		counter=$((counter+1))
 	fi	
