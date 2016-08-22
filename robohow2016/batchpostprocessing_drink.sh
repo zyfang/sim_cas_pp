e9779232a8425b2d42c60ada08b1dbd4391d5992
zyfang <yzfang1@gmail.com>
2016-08-22 16:58:21 +0200

    changed batch script to get order and subj from the filepath and use this in the collectionname rather than the dbname

--- robohow2016/batchpostprocessing_drink.sh
+++ robohow2016/batchpostprocessing_drink.sh
@@ -7,11 +7,13 @@ for D in `find $DATA_IN_DIR_FULLPATH -type d`
 do
 	filepath=$D"/state.log"
 	#collectionname=`echo $filepath | cut -d '_' -f 4``echo $filepath | cut -d '_' -f 5`
-	re="\/$RUN_NAME\/(.*)\/.*\/gzserver\/state\.log"
+	re="$EXP_NAME\/(.+)\_(.+)\/(.+)\/.+\/gzserver\/state\.log"
 	if [[ $filepath =~ $re ]]; then 
 
-		collectionname=${BASH_REMATCH[1]};
-		# echo $filepath" to "$collectionname
-		gzserver --verbose -s libPostProcess.so -p $filepath --config config_drink.cfg --db $EXP_NAME\_$RUN_NAME --collection $collectionname --suffix 0 --delay 2 -replaying 
+		order=${BASH_REMATCH[1]};
+		subj=${BASH_REMATCH[2]};
+		collectionname=${BASH_REMATCH[3]};
+		# echo $filepath" to "$order\_$subj\_$collectionname
+		gzserver --verbose -s libPostProcess.so -p $filepath --config config_drink.cfg --db $EXP_NAME --collection $order\_$subj\_$collectionname --suffix 0 --delay 2 -replaying 
 	fi	
 done
