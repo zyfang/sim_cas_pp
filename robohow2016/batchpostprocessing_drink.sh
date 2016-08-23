659b07c4b8b97d8bbbadbcba26a58b8cb050d398
zyfang <yzfang1@gmail.com>
2016-08-23 16:09:24 +0200

    added option to batch script to skip demonstrations by defining order, subj and isim in a file

--- robohow2016/batchpostprocessing_drink.sh
+++ robohow2016/batchpostprocessing_drink.sh
@@ -1,8 +1,8 @@
-RUN_NAME="A_yf1"
-EXP_NAME="param_test"
+# RUN_NAME="A_yf1"
+EXP_NAME="run1"
 DATA_IN_DIR_FULLPATH="/media/yfang/hdd3/HumanData/Robohow1/logs_drink/"$EXP_NAME"/"
+EXCLUSION_FILE="/home/yfang/workspaces/catkin_ws/src/sim_cas_pp/bad_demos.csv"
 
-echo $RUN_NAME
 for D in `find $DATA_IN_DIR_FULLPATH -type d`
 do
 	filepath=$D"/state.log"
@@ -13,7 +13,16 @@ do
 		order=${BASH_REMATCH[1]};
 		subj=${BASH_REMATCH[2]};
 		collectionname=${BASH_REMATCH[3]};
+		while IFS=, read col1 col2 col3
+		do
+			if [[ $order == $col1 ]] && [[ $subj == $col2 ]] && [[ $collectionname == $col3 ]]; then
+				echo $order\_$subj\_$collectionname" skipped because in bad_demos.csv"
+			else
+				gzserver --verbose -s libPostProcess.so -p $filepath --config config_drink.cfg --db $EXP_NAME --collection $order\_$subj\_$collectionname --suffix 0 --delay 2 -replaying 
+			fi	
+		done < $EXCLUSION_FILE
+
 		# echo $filepath" to "$order\_$subj\_$collectionname
-		gzserver --verbose -s libPostProcess.so -p $filepath --config config_drink.cfg --db $EXP_NAME --collection $order\_$subj\_$collectionname --suffix 0 --delay 2 -replaying 
+		# gzserver --verbose -s libPostProcess.so -p $filepath --config config_drink.cfg --db $EXP_NAME --collection $order\_$subj\_$collectionname --suffix 0 --delay 2 -replaying 
 	fi	
 done
